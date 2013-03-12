9c0c8352f4750b851bcfab2300f2f91f0434a58d
lincolnge <326684793@qq.com>
2013-03-12 23:08:45 +0800

    update

---
+++ readme.txt
@@ -0,0 +1,3 @@
+这算是我第一个Android程序，于是发上来大家一起看看！写得很差，没有注释，并且有一堆多余的东西。
+
+This is my first Android Programme. 
---
+++ src/hello/tab/AlbumsActivity.java
@@ -0,0 +1,74 @@
+package hello.tab;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class AlbumsActivity extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.albums);
+
+	    Gallery gallery = (Gallery) findViewById(R.id.gallery);
+	    gallery.setAdapter(new ImageAdapter(this));
+
+	    gallery.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView parent, View v, int position, long id) {
+	            Toast.makeText(AlbumsActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	}
+	
+	public class ImageAdapter extends BaseAdapter {
+	    int mGalleryItemBackground;
+	    private Context mContext;
+
+	    private Integer[] mImageIds = {
+	            R.drawable.pic_3,
+//	            R.drawable.pic_4,
+//	            R.drawable.pic_14,
+	    };
+
+	    public ImageAdapter(Context c) {
+	        mContext = c;
+	        TypedArray attr = mContext.obtainStyledAttributes(R.styleable.HelloGallery);
+	        mGalleryItemBackground = attr.getResourceId(
+	                R.styleable.HelloGallery_android_galleryItemBackground, 0);
+	        attr.recycle();
+	    }
+
+	    public int getCount() {
+	        return mImageIds.length;
+	    }
+
+	    public Object getItem(int position) {
+	        return position;
+	    }
+
+	    public long getItemId(int position) {
+	        return position;
+	    }
+
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        ImageView imageView = new ImageView(mContext);
+
+	        imageView.setImageResource(mImageIds[position]);
+//	        imageView.setLayoutParams(new Gallery.LayoutParams(150, 100));
+	        imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+	        imageView.setBackgroundResource(mGalleryItemBackground);
+
+	        return imageView;
+	    }
+	}
+}
